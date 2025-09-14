2f7c8c4c5608b874f522e3177b558f8d84c3fee2
VagrantAC <vagrantac@126.com>
2025-09-15 00:52:53 +0800

    first commit

--- examples/main.py
+++ examples/main.py
@@ -5,9 +5,9 @@ iris_dataset = load_iris()
 print("Keys of iris_dataset: \n{}".format(iris_dataset.keys()))
 
 # DESCR 数据的描述
-print(iris_dataset['DESCR'][:193] + "\n...")
+print(iris_dataset["DESCR"][:193] + "\n...")
 
 # target_names 花的种类
-print("Target names: {}".format(iris_dataset['target_names']))
+print("Target names: {}".format(iris_dataset["target_names"]))
 
-# 
+#
